d89deaac2e1799f9cc7b98cd508ada06331d890d
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2021-12-08 16:01:31 +0100

    Creating Calculation, history, history of all users and fill tables.
    -Creating and connecting history to main menu
    -Creating and connecting history of all users to admin menu
    -Creating Class to populate tables - list of users, history and history of all users
    -Creating Calculation

---
+++ src/Utilities/FillTable.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author kcfke
+ */
+public class FillTable {
+
+    /**
+     * This method will populate the tables in the ListOfUsers, History and
+     * HistoryOfAllUsers frames.
+     *
+     * @param table
+     * @param Query
+     */
+    public static void FillTable(JTable table, String Query) throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "root");
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(Query);
+
+            //To remove previously added rows
+            while (table.getRowCount() > 0) {
+                ((DefaultTableModel) table.getModel()).removeRow(0);
+            }
+            int columns = rs.getMetaData().getColumnCount();
+            while (rs.next()) {
+                Object[] row = new Object[columns];
+                for (int i = 1; i <= columns; i++) {
+                    row[i - 1] = rs.getObject(i);
+                }
+                ((DefaultTableModel) table.getModel()).insertRow(rs.getRow() - 1, row);
+            }
+
+            rs.close();
+            stat.close();
+            conn.close();
+        } catch (SQLException e) {
+        }
+    }
+
+}
